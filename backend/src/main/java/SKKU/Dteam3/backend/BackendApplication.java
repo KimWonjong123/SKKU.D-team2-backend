51da713845acda9b0a20139908867f9b36f11e4b
KimWonjong123 <bonk159@gmail.com>
2023-10-08 14:39:00 +0900

    initial commit

---
+++ backend/src/main/java/SKKU/Dteam3/backend/BackendApplication.java
@@ -0,0 +1,13 @@
+package SKKU.Dteam3.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/SKKU/Dteam3/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package SKKU.Dteam3.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
