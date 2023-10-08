48f5ecca5e6f830cc64fe23f50647e6233605c26
DooAnyThing <125381163+DooAnyThing@users.noreply.github.com>
2023-10-08 18:03:34 +0900

    투두모임, 모임썸네일, 모임멤버, 오늘의 한마디 도메인추가

---
+++ backend/src/main/java/SKKU/Dteam3/backend/domain/GatheringMember.java
@@ -0,0 +1,23 @@
+package SKKU.Dteam3.backend.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class GatheringMember {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Gathering gathering;
+
+}
---
+++ backend/src/main/java/SKKU/Dteam3/backend/domain/WordOfTheDay.java
@@ -0,0 +1,29 @@
+package SKKU.Dteam3.backend.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class WordOfTheDay {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id")
+    private User user;
+
+    private LocalDateTime date;
+
+    private String content = "";
+
+    private String position; //아직 저장 방식 협의 안됨
+
+}
