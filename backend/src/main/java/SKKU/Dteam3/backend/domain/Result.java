ce8fb49acbb5631eb15ad286be90c29a068bc2ef
KimWonjong123 <bonk159@gmail.com>
2023-10-08 22:21:47 +0900

    result 도메인 추가

---
+++ backend/src/main/java/SKKU/Dteam3/backend/domain/Result.java
@@ -0,0 +1,34 @@
+package SKKU.Dteam3.backend.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Result {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private boolean isDone;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "todo_id", nullable = false)
+    private Todo todo;
+
+    public Result(User user, Todo todo) {
+        this.isDone = false;
+        this.user = user;
+        this.todo = todo;
+    }
+}
