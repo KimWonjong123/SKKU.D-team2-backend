f9c08d9e084ce1d9c8a138b3eca54a250db4337a
KimWonjong123 <bonk159@gmail.com>
2023-11-07 15:54:57 +0900

    GuestBook table 추가

---
+++ backend/src/main/java/SKKU/Dteam3/backend/domain/GuestBook.java
@@ -0,0 +1,48 @@
+package SKKU.Dteam3.backend.domain;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class GuestBook {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    @NotNull
+    private User user;
+
+    @NotNull
+    @Temporal(TemporalType.DATE)
+    private LocalDate date;
+
+    @NotNull
+    private String content;
+
+    @NotNull
+    private String position;
+
+    @NotNull
+    private String font;
+
+    @NotNull
+    private int fontSize;
+
+    public GuestBook(User user, String content, String position, String font, int fontSize) {
+        this.user = user;
+        this.date = LocalDate.now();
+        this.content = content;
+        this.position = position;
+        this.font = font;
+        this.fontSize = fontSize;
+    }
+}
