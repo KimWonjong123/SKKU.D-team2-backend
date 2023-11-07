ed30225255e61c9815b2710494aec83bfcdf985a
KimWonjong123 <bonk159@gmail.com>
2023-11-07 15:23:13 +0900

    memo 테이블 수정

--- backend/src/main/java/SKKU/Dteam3/backend/domain/Memo.java
+++ backend/src/main/java/SKKU/Dteam3/backend/domain/Memo.java
@@ -1,6 +1,7 @@
 package SKKU.Dteam3.backend.domain;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,7 +11,7 @@ import java.time.LocalDateTime;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class WordOfTheDay {
+public class Memo {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -20,16 +21,27 @@ public class WordOfTheDay {
     @JoinColumn(name = "user_id")
     private User user;
 
+    @NotNull
     private LocalDateTime date;
 
+    @NotNull
     private String content = "";
 
+    @NotNull
     private String position; //아직 저장 방식 협의 안됨
 
-    public WordOfTheDay(User user, String content, String position) {
+    @NotNull
+    private String font;
+
+    @NotNull
+    private int fontSize;
+
+    public Memo(User user, String content, String position, String font, int fontSize) {
         this.user = user;
         this.content = content;
         this.position = position;
+        this.font = font;
+        this.fontSize = fontSize;
     }
 
     //비지니스 로직
