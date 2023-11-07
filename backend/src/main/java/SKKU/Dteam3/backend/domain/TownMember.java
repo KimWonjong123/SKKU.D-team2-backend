3b6d74706175e22034035bff7b14efe4a8b86144
KimWonjong123 <bonk159@gmail.com>
2023-11-07 15:39:01 +0900

    TownMember 테이블 이름 수정

--- backend/src/main/java/SKKU/Dteam3/backend/domain/TownMember.java
+++ backend/src/main/java/SKKU/Dteam3/backend/domain/TownMember.java
@@ -1,6 +1,7 @@
 package SKKU.Dteam3.backend.domain;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,41 +9,43 @@ import lombok.NoArgsConstructor;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class GatheringMember {
+public class TownMember {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
+    @NotNull
     private User user;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    private Gathering gathering;
+    @NotNull
+    private Town town;
 
     //비지니스 로직
-    public GatheringMember(User user, Gathering gathering) {
+    public TownMember(User user, Town town) {
         this.user = user;
-        this.gathering = gathering;
+        this.town = town;
     }
 
-    public boolean recruitNewMember(User user, Gathering gathering){
+    public boolean recruitNewMember(User user, Town town){
 
-        if(!gathering.increaseMemberNum()){ //정원이 가득 찬 경우
+        if(!town.increaseMemberNum()){ //정원이 가득 찬 경우
             //alertErrorMessage("member quota is full");
             return false;
         }
         //check duplicate from repository
-        GatheringMember gatheringMember = new GatheringMember(user, gathering);
+        TownMember townMember = new TownMember(user, town);
         //save to repository
 
         return true;
     }
 
-    public boolean removeMemberByLeader(User user, Gathering gathering){ //리스트는 팀원만 뜨게
+    public boolean removeMemberByLeader(User user, Town town){ //리스트는 팀원만 뜨게
 
         //check exist from repository
-        if(!gathering.decreaseMemberNum(user)) return false;
+        if(!town.decreaseMemberNum(user)) return false;
         else{
             //remove from repository
             return true;
