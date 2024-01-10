ff65dc56acfded7bf1605c48b95afd949c714aaa
Wonjong Kim <bonk159@gmail.com>
2024-01-11 01:14:51 +0900

    feat: validation exception handling 추가

--- backend/src/main/java/SKKU/Dteam3/backend/controller/UserController.java
+++ backend/src/main/java/SKKU/Dteam3/backend/controller/UserController.java
@@ -1,24 +1,23 @@
 package SKKU.Dteam3.backend.controller;
 
 import SKKU.Dteam3.backend.domain.TodoDetail;
-import SKKU.Dteam3.backend.domain.Town;
 import SKKU.Dteam3.backend.domain.User;
 import SKKU.Dteam3.backend.dto.*;
 import SKKU.Dteam3.backend.service.UserService;
-import lombok.NoArgsConstructor;
+import jakarta.validation.constraints.PastOrPresent;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
-import java.security.Principal;
 import java.time.LocalDate;
-import java.util.List;
 import java.util.Objects;
 
 @RestController
 @RequestMapping("/users")
 @RequiredArgsConstructor
+@Validated
 public class UserController {
 
         private final UserService userService;
@@ -32,7 +31,7 @@ public class UserController {
         @ResponseStatus(HttpStatus.OK)
         public MemoResponseDto getMemo(@PathVariable Long userId,
                                        Authentication authentication,
-                                       @RequestParam(required = false) LocalDate date) {
+                                       @RequestParam(required = false) @PastOrPresent LocalDate date) {
                 return userService.getMemo(
                         userId,
                         (User) authentication.getPrincipal(),
@@ -43,7 +42,7 @@ public class UserController {
         @GetMapping("/me/memo")
         @ResponseStatus(HttpStatus.OK)
         public MemoResponseDto getMemo(Authentication authentication,
-                                       @RequestParam(required = false) LocalDate date) {
+                                       @RequestParam(required = false) @PastOrPresent LocalDate date) {
                 User user = (User) authentication.getPrincipal();
                 return userService.getMyMemo(
                         user,
@@ -63,7 +62,7 @@ public class UserController {
         @ResponseStatus(HttpStatus.OK)
         public ListDto<TodoDetail> getTodo(Authentication authentication,
                                            @PathVariable Long userId,
-                                           @RequestParam(required = false) LocalDate date) {
+                                           @RequestParam(required = false) @PastOrPresent LocalDate date) {
                 return userService.getTodo(
                         userId,
                         (User) authentication.getPrincipal(),
@@ -74,7 +73,7 @@ public class UserController {
         @GetMapping("/me/todo")
         @ResponseStatus(HttpStatus.OK)
         public ListDto<TodoDetail> getTodo(Authentication authentication,
-                                           @RequestParam(required = false) LocalDate date) {
+                                           @RequestParam(required = false) @PastOrPresent  LocalDate date) {
                 return userService.getMyTodo(
                         (User) authentication.getPrincipal(),
                         Objects.requireNonNullElseGet(date, LocalDate::now)
@@ -92,7 +91,7 @@ public class UserController {
         @ResponseStatus(HttpStatus.OK)
         public AchieveResponseDto getAchieve(@PathVariable Long userId,
                                              Authentication authentication,
-                                             @RequestParam(required = false) LocalDate date) {
+                                             @RequestParam(required = false) @PastOrPresent LocalDate date) {
                 return userService.getAchieve(
                         userId,
                         (User) authentication.getPrincipal(),
@@ -103,7 +102,7 @@ public class UserController {
         @GetMapping("/me/achieve")
         @ResponseStatus(HttpStatus.OK)
         public AchieveResponseDto getAchieve(Authentication authentication,
-                                             @RequestParam(required = false) LocalDate date) {
+                                             @RequestParam(required = false) @PastOrPresent LocalDate date) {
                 User user = (User) authentication.getPrincipal();
                 return userService.getMyAchieve(
                         user,
@@ -115,7 +114,7 @@ public class UserController {
         @ResponseStatus(HttpStatus.OK)
         public ListDto<AchieveResponseDto> getCalendar(@PathVariable Long userId,
                                                        Authentication authentication,
-                                                       @RequestParam LocalDate date) {
+                                                       @RequestParam(required = false) @PastOrPresent LocalDate date) {
                 return userService.getCalendar(
                         userId,
                         (User) authentication.getPrincipal(),
@@ -126,7 +125,7 @@ public class UserController {
         @GetMapping("/me/calendar")
         @ResponseStatus(HttpStatus.OK)
         public ListDto<AchieveResponseDto> getCalendar(Authentication authentication,
-                                                       @RequestParam LocalDate date) {
+                                                       @RequestParam(required = false) @PastOrPresent LocalDate date) {
                 return userService.getMyCalendar(
                         (User) authentication.getPrincipal(),
                         Objects.requireNonNullElseGet(date, LocalDate::now)
