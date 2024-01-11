91b9123bffb7501267accae7f5008ed230f09b8d
kimkisu2502 <kimkisu2502@g.skku.edu>
2024-01-11 18:06:45 +0900

    feat: 알람 도메인 추가

---
+++ backend/src/main/java/SKKU/Dteam3/backend/domain/Alarm.java
@@ -0,0 +1,38 @@
+package SKKU.Dteam3.backend.domain;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Alarm {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JoinColumn(name = "to_user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JoinColumn(name = "from_user_id")
+    private User fromUser;
+
+    @NotNull
+    private String content;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private LocalDateTime createdAt;
+
+}
